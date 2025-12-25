7c06fab0303653e1faeabc0bdc8640850b15e87a
peeryklishtml <willyanpiklis@gmail.com>
2025-12-25 00:46:03 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import { Layout } from './components/Layout';
@@ -6,14 +6,21 @@ import { Home } from './pages/Home';
 import { Product } from './pages/Product';
 import { Cart } from './pages/Cart';
 import { Checkout } from './pages/Checkout';
+import { initFacebookPixel } from './lib/facebook';
 
 function App() {
+  useEffect(() => {
+    // initialize pixel on app load
+    initFacebookPixel();
+  }, []);
+
   return (
     <CartProvider>
       <BrowserRouter>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Home />} />
             <Route path="/product/:id" element={<Product />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<Checkout />} />
